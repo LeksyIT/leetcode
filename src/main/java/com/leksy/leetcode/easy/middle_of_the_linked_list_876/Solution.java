1da9535dc7795e633e35b87a2c14d999f29a3c62
Viktor Anohin <V.Anokhin@hostco.ru>
2022-10-27 22:50:15 +0300

    problem 876

--- src/main/java/com/leksy/leetcode/easy/middle_of_the_linked_list_876/Solution.java
+++ src/main/java/com/leksy/leetcode/easy/middle_of_the_linked_list_876/Solution.java
@@ -1,14 +1,9 @@
 package com.leksy.leetcode.easy.middle_of_the_linked_list_876;
 
 /**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
+ * Definition for singly-linked list. public class ListNode { int val; ListNode next; ListNode() {}
+ * ListNode(int val) { this.val = val; } ListNode(int val, ListNode next) { this.val = val;
+ * this.next = next; } }
  */
 class Solution {
 
@@ -19,16 +14,15 @@ class Solution {
   }
 
   public static ListNode middleNode(ListNode head) {
-    int count = 1;
-    ListNode next = head.next;
-    while (next != null){
-      next = next.next;
-      count+=1;
+    if (head.next == null) {
+      return head;
     }
-    count = count/2;
-    for (int i = 0; i < count; i++) {
-      head = head.next;
+    ListNode slow = head;
+    ListNode fast = head;
+    while (fast != null && fast.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
     }
-    return head;
+    return slow;
   }
 }
