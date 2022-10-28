c374ca01c104375d39cd0d99b72cf7bf4e4dfee7
Viktor Anohin <V.Anokhin@hostco.ru>
2022-10-29 00:33:14 +0300

    problem 121

---
+++ src/main/java/com/leksy/leetcode/easy/best_time_to_buy_and_sell_stock_121/Solution.java
@@ -0,0 +1,44 @@
+package com.leksy.leetcode.easy.best_time_to_buy_and_sell_stock_121;
+
+public class Solution {
+
+  public static void main(String[] args) {
+    System.out.println(maxProfit(new int[]{7,1,5,3,6,4}));
+  }
+
+//  public int maxProfit(int[] prices) {
+//    int lsf = Integer.MAX_VALUE;
+//    int op = 0;
+//    int pist = 0;
+//
+//    for(int i = 0; i < prices.length; i++){
+//      if(prices[i] < lsf){
+//        lsf = prices[i];
+//      }
+//      pist = prices[i] - lsf;
+//      if(op < pist){
+//        op = pist;
+//      }
+//    }
+//    return op;
+//  }
+
+  public static int maxProfit(int[] prices) {
+    int buy = 0;
+    int sell = 1;
+    int maxProfit = 0;
+    while (sell<prices.length){
+      int currentProfit = prices[sell] - prices[buy];
+      if (prices[sell] > prices[buy]){
+        if (maxProfit<currentProfit){
+          maxProfit=currentProfit;
+        }
+      } else {
+        buy = sell;
+      }
+
+      sell+=1;
+    }
+    return maxProfit;
+  }
+}
