5bba45bf445a257cf862024787b6cf6550a6d3a6
Viktor Anohin <V.Anokhin@hostco.ru>
2022-10-30 00:09:34 +0300

    problem 704

--- src/main/java/com/leksy/leetcode/easy/binary_search_704/Solution.java
+++ src/main/java/com/leksy/leetcode/easy/binary_search_704/Solution.java
@@ -3,27 +3,21 @@ package com.leksy.leetcode.easy.binary_search_704;
 class Solution {
 
   public static void main(String[] args) {
-    System.out.println(search(new int[]{-1,0,3,5,9,12}, 9));
+    System.out.println(search(new int[]{-1, 0, 3, 5, 9, 12}, 0));
   }
 
   public static int search(int[] nums, int target) {
-    if (nums.length == 1) {
-      if (nums[0] == target) {
-        return 0;
-      } else {
-        return -1;
+    int low = 0;
+    int high = nums.length-1;
+    while (low <= high) {
+      int middle = (low + high) / 2;
+      if (middle == target) {
+        return middle;
       }
-    }
-    int length = nums.length/2;
-    int aim = (int) (Math.log(nums.length) / Math.log(2));
-    for (int i = 0; i < aim; i++) {
-      if (nums[length] == target) {
-        return length;
-      }
-      if (length > target) {
-        length = length - length/2;
+      if (middle < target) {
+        low = middle + 1;
       } else {
-        length = length + length/2;
+        high = middle - 1;
       }
     }
     return -1;
