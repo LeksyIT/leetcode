1c06604960690ab2dae759cf9181f1a44d28798c
Viktor Anohin <V.Anokhin@hostco.ru>
2022-11-02 21:57:32 +0300

    problem 189

---
+++ src/main/java/com/leksy/leetcode/easy/rotate_array_189/Solution.java
@@ -0,0 +1,20 @@
+package com.leksy.leetcode.easy.rotate_array_189;
+
+public class Solution {
+
+  public static void main(String[] args) {
+    rotate(new int[]{1,2,3},1);
+  }
+
+  public static void rotate(int[] nums, int k) {
+    int a = nums.length;
+    if (a != 1 && k != 0 && k != a) {
+      int[] ans = new int[a];
+      System.arraycopy(nums, a - k % a, ans, 0, k % a);
+      System.arraycopy(nums, 0, ans, k % a, a - k % a);
+      for (int i = 0; i < a; i++) {
+        nums[i] = ans[i];
+      }
+    }
+  }
+}
